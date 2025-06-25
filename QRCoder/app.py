fccf5ec20d6b3a9bcb09e71dba4bb37f8aa28b89
WENEMOUS <novus.lordo.seclorum@gmail.com>
2025-06-25 12:40:28 +0300

    QRCoder: App: Set imports
    Signed-off-by: WENEMOUS <novus.lordo.seclorum@gmail.com>

---
+++ QRCoder/app.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+import qrcode
+from PyQt6.QtWidgets import (QApplication, QWidget, QTableView, QHeaderView,
+                             QVBoxLayout, QPushButton, QFileDialog,
+                             QAbstractItemView, QDialog, QLabel, QHBoxLayout)
+from PyQt6.QtGui import QStandardItemModel, QStandardItem, QPixmap, QIcon
+from PyQt6.QtCore import Qt
